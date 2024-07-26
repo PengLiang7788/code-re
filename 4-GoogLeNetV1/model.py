a495aeb49d8e1bdde09787f3b9443efcd470cd6d
PengLiang <1729631268@qq.com>
2024-07-26 21:06:29 +0800

    model fix

--- 4-GoogLeNetV1/model.py
+++ 4-GoogLeNetV1/model.py
@@ -7,16 +7,44 @@ class GoogLeNet(nn.Module):
     def __init__(self, num_classes=1000, aux_logits=False, init_weights=False):
         super(GoogLeNet, self).__init__()
         self.conv1 = BasicConv2d(3, 64, kernel_size =7, stride=2, padding=2)
-        # ceil_mode=True向上取整
         self.maxpool1 = nn.MaxPool2d(kernel_size=3, stride=2, ceil_mode=True)
-        self.conv2 = BasicConv2d(64, 64, kernel_size=1)
+
+        self.conv2 = BasicConv2d(64, 64, kernel_size=1) 
         self.conv3 = BasicConv2d(64, 192, kernel_size=3, stride=1, padding=1)
         self.maxpool2 = nn.MaxPool2d(kernel_size=3, stride=2, ceil_mode=True)
 
+        self.inception3a = Inception(192, 64, 96, 128, 16, 32, 32)
+        
 
 class Inception(nn.Module):
-    def __init__(self):
+    def __init__(self, in_channels, ch1x1, ch3x3red, ch3x3, ch5x5red, ch5x5, pool_proj):
         super(Inception, self).__init__()
+        self.branch1 = BasicConv2d(in_channels, ch1x1, kernel_size=1)
+
+        self.branch2 = nn.Sequential(
+            BasicConv2d(in_channels, ch3x3red, kernel_size=1),
+            BasicConv2d(ch3x3red, ch3x3, kernel_size=3, padding=1)
+        )
+
+        self.branch3 = nn.Sequential(
+            BasicConv2d(in_channels, ch5x5red, kernel_size=1),
+            BasicConv2d(ch5x5red, ch5x5, kernel_size=3, padding=2)
+        )
+
+        self.branch4 = nn.Sequential(
+            nn.MaxPool2d(kernel_size=3, stride=1, padding=1),
+            BasicConv2d(in_channels, pool_proj, kernel_size=1)
+        )
+    
+    def forward(self, x):
+        branch1 = self.branch1(x)
+        branch2 = self.branch2(x)
+        branch3 = self.branch3(x)
+        branch4 = self.branch4(x)
+
+        outputs = [branch1, branch2, branch3, branch4]
+        return torch.cat(outputs, 1)
+
 
 
 class BasicConv2d(nn.Module):
