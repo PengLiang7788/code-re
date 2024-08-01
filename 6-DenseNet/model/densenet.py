acd9e8a3faa91e172d20f500bae1bf609fc549db
PengLiang <1729631268@qq.com>
2024-08-01 18:54:04 +0800

    BottleNck

--- 6-DenseNet/model/densenet.py
+++ 6-DenseNet/model/densenet.py
@@ -50,6 +50,34 @@ class TransitionBlock(nn.Module):
         # 过渡层不需要进行稠密连接
         return out
         
+# BottleNeck layer 瓶颈层, 在 3x3 卷积层之前使用 1x1 卷积降维
+class BottleNeck(nn.Module):
+    def __init__(self, in_channel, out_channel, dropout_rate=0.0):
+        super(BottleNeck, self).__init__()
+        self.bn1 = nn.BatchNorm2d(in_channel)
+        # 使用1x1卷积降维, 生成 4k 个特征图, k=out_channel
+        self.relu = nn.ReLU(inplace=True)
+        self.conv1 = nn.Conv2d(in_channels=in_channel, out_channels=4 * out_channel,
+                              kernel_size=1, stride=1, padding=0, bias=False)
+        self.bn2 = nn.BatchNorm2d(4 * out_channel)
+        self.conv2 = nn.Conv2d(in_channels=4 * out_channel, out_channels=out_channel, 
+                               kernel_size=3, stride=1, padding=1, bias=False)
+        self.droprate = dropout_rate
+        
+    def forward(self, x):
+        # 所有卷积操作之前都会进行 BN 和 ReLU
+        out = self.conv1(self.relu(self.bn1(x)))
+        if self.droprate > 0:
+            out = F.dropout(out, p=self.droprate, training=self.training)
+        out = self.conv2(self.relu(self.bn2(out)))
+        if self.droprate > 0:
+            out = F.dropout(out, p=self.droprate, training=self.training)
+        
+        return torch.cat([x, out], 1)
+
+class DenseBlock(nn.Module):
+    def __init__(self):
+        super(DenseBlock, self).__init__()
 
 class DenseNet(nn.Module):
     def __init__(self):
