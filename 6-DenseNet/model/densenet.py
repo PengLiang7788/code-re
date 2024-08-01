bbb50b82eea8da8ae0c8ccb834accda301b24e8a
PengLiang <1729631268@qq.com>
2024-08-01 23:18:39 +0800

    densenet

--- 6-DenseNet/model/densenet.py
+++ 6-DenseNet/model/densenet.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import math
 
 
 # 对应论文中 Composit function
@@ -72,7 +73,7 @@ class BottleNeck(nn.Module):
         return torch.cat([x, out], 1)
 
 class DenseBlock(nn.Module):
-    def __init__(self, block, nb_layers, in_channel, growth_rate, drop_rate=0.0):
+    def __init__(self, block, nb_layers, in_channels, growth_rate, drop_rate=0.0):
         """
         Args:
             block: BasicBlock/BottleNeck
@@ -82,19 +83,95 @@ class DenseBlock(nn.Module):
             drop_rate: dropout rate
         """
         super(DenseBlock, self).__init__()
-        self.layer = self._make_layer(block, in_channel, growth_rate, nb_layers, drop_rate)
+        self.layer = self._make_layer(block, in_channels, growth_rate, nb_layers, drop_rate)
     
-    def _make_layer(self, block, in_channel, growth_rate, nb_layers, drop_rate):
+    def _make_layer(self, block, in_channels, growth_rate, nb_layers, drop_rate):
         layers = []
         for i in range(nb_layers):
             # 每一层的输出都是 growth_rate 个通道, 块中前面块的输出作为后面块的输入
-            layers.append(block(in_channel + i * growth_rate, growth_rate, drop_rate))
+            layers.append(block(in_channels + i * growth_rate, growth_rate, drop_rate))
 
         return nn.Sequential(*layers)
 
     def forward(self, x):
         return self.layer(x)
 
+# 适用于出ImageNet数据集之外的DenseNet结构
 class DenseNet(nn.Module):
-    def __init__(self):
+    def __init__(self, depth, num_classes=10, growth_rate=12, 
+                 reduction=0.5, bottleNeck=True, drop_rate=0.0):
+        """
+        Args:
+            depth: 模型深度
+            num_classes: 数据集类别数
+            growth_rate: 模型增长率
+            reduction: 压缩因子
+            bottleNeck: BottleNeck/BasicBlock, default BottleNeck
+            drop_rate: dropout rate
+        """
         super(DenseNet, self).__init__()
+        # DenseNet-BC第一个卷积层输出通道数为两倍的增长率
+        in_channel = 2 * growth_rate
+        # 三个密集块拥有相同的层数
+        # 第一个卷积层, 最后一个全连接层, 中间两个过渡层, 每个过渡层拥有一个权重层,
+        # conv -> DenseBlock -> TransitionLayer -> DenseBlock -> TransitionLayer -> DenseBlock -> FullyConnected
+        # 稠密块总层数为 depth - 4, 每个稠密块层数 (depth - 4) / 3
+        n = (depth - 4) / 3
+        if bottleNeck == True:
+            # 每个BottleNeck有两个权重层
+            # 稠密块中BottleNeck数量
+            n = n / 2
+            block = BottleNeck
+        else:
+            # 每个BasicBlock中只有一个权重层
+            block = BasicBlock
+        n = int(n)
+        # 1st conv before any dense block
+        # 输入图像 3x32x32, 每个3x3卷积使用1个像素填充0, 且保持输出特征图大小不变
+        self.conv1 = nn.Conv2d(3, in_channel, kernel_size=3, 
+                               stride=1, padding=1, bias=False)
+        # 1st block
+        self.block1 = DenseBlock(block, n, in_channel, growth_rate, drop_rate)
+        in_channel = int(in_channel + n * growth_rate)
+        # DenseNet-BC 中间层生成 (θ * m)(向下取整)个输出特征图, 其中m表示上一个稠密层输出特征图数
+        self.trans1 = TransitionBlock(in_channel, int(math.floor(reduction * in_channel)), drop_rate)
+        in_channel = int(math.floor(reduction * in_channel))
+        # 2nd block
+        self.block2 = DenseBlock(block, n, in_channel, growth_rate, drop_rate)
+        in_channel = int(in_channel + n * growth_rate)
+        self.trans2 = TransitionBlock(in_channel, int(math.floor(reduction * in_channel)), drop_rate)
+        in_channel = int(math.floor(reduction * in_channel))
+        # 3rd block
+        self.block3 = DenseBlock(block, n, in_channel, growth_rate, drop_rate)
+        in_channel = int(in_channel + n * growth_rate)
+        # 全局平均池化
+        self.bn = nn.BatchNorm2d(in_channel)
+        self.relu = nn.ReLU(inplace=True)
+        self.avgpool = nn.AvgPool2d(kernel_size=8)
+        # 全连接层
+        self.fc = nn.Linear(in_channel, num_classes)
+        self.in_channel = in_channel
+
+        # 初始化权重
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+                m.weight.data.normal_(0, math.sqrt(2. / n))
+            elif isinstance(m, nn.BatchNorm2d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
+            elif isinstance(m, nn.Linear):
+                m.bias.data.zero_()
+
+    def forward(self, x):
+        out = self.conv1(x)
+        out = self.trans1(self.block1(out))
+        
+        out = self.trans2(self.block2(out))
+        
+        out = self.block3(out)
+        out = self.relu(self.bn(out))
+        out = self.avgpool(out)
+        out = torch.flatten(out, 1)
+        return self.fc(out)
+        
