d80c38a837acaa61a11f1257bbe231554859cb47
PengLiang <1729631268@qq.com>
2024-08-14 19:09:49 +0800

    ParNet

--- 9-ParNet/model/net.py
+++ 9-ParNet/model/net.py
@@ -1,23 +1,23 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from typing import Optional, Callable
+from typing import Optional, Callable, List
 from functools import partial
 
 
 class SSEBlock(nn.Module):
-    def __init__(self, in_planes: int):
+    def __init__(self, in_planes: int, out_planes: int):
         # SSE模块一个分支只进行BatchNorm, 输入特征矩阵形状不会改变
         # SSE另一个分支会先进行全局平均池化, 然后使用1x1卷积, 其中输出通道数仅受1x1卷积影响
         # SSE模块最好需要将两个分支的输出结果进行相乘再输出, 为保证输出结果相同, 1x1卷积的输入通道数应当等于输出通道数
         super(SSEBlock, self).__init__()
         self.norm = nn.BatchNorm2d(in_planes)
         self.pool = nn.AdaptiveAvgPool2d((1, 1))
-        self.fc = nn.Conv2d(in_channels=in_planes, out_channels=in_planes, kernel_size=1, bias=False)
+        self.fc = nn.Conv2d(in_channels=in_planes, out_channels=out_planes, kernel_size=1, bias=False)
 
     def forward(self, x):
         bn = self.norm(x)
-        output = F.sigmoid(self.fc(self.pool(x)))
+        output = torch.sigmoid(self.fc(self.pool(x)))
 
         return torch.mul(bn, output)
 
@@ -28,6 +28,12 @@ class DownSampling(nn.Module):
         self.branch1 = nn.Sequential(
             # 图像长宽减半, AvgPool2d中padding默认等于kernel_size
             nn.AvgPool2d(kernel_size=2),
+            nn.Conv2d(in_planes, out_planes, kernel_size=1, bias=False),
+            nn.BatchNorm2d(out_planes)
+        )
+        self.branch1_last = nn.Sequential(
+            # 最后一个downsampling层, 输入图像为7x7, 为了使得与branch2输出图像尺寸相同, 设置padding=1
+            nn.AvgPool2d(kernel_size=2, padding=1),
             nn.Conv2d(in_planes, out_planes, kernel_size=1, bias=False),
             nn.BatchNorm2d(out_planes)
         )
@@ -44,7 +50,11 @@ class DownSampling(nn.Module):
         )
 
     def forward(self, x):
-        branch1 = self.branch1(x)
+        b, c, h, w = x.data.size()
+        if h % 2 == 1:
+            branch1 = self.branch1_last(x)
+        else:
+            branch1 = self.branch1(x)
         branch2 = self.branch2(x)
         branch3 = self.branch3(x)
 
@@ -59,21 +69,7 @@ class FusionBlock(nn.Module):
         self.bn = nn.BatchNorm2d(in_planes)
         # 由于在最开始有一个concatenation, 导致Fusion模块内通道数翻倍
         self.groups = in_planes
-        hidden_channel = in_planes * 2
-        self.branch1 = nn.Sequential(
-            nn.AvgPool2d(kernel_size=2),
-            nn.Conv2d(hidden_channel, out_planes, kernel_size=1, groups=2, stride=1, bias=False),
-            nn.BatchNorm2d(out_planes)
-        )
-        self.branch2 = nn.Sequential(
-            nn.Conv2d(hidden_channel, out_planes, kernel_size=3, stride=2, groups=2, padding=1, bias=False),
-            nn.BatchNorm2d(out_planes)
-        )
-        self.branch3 = nn.Sequential(
-            nn.AdaptiveAvgPool2d((1, 1)),
-            nn.Conv2d(hidden_channel, out_planes, kernel_size=1, groups=2, bias=False),
-            nn.Sigmoid()
-        )
+        self.downsample = DownSampling(in_planes * 2, out_planes)
 
     # 通道混洗
     def channel_shuffle(self, x):
@@ -90,12 +86,139 @@ class FusionBlock(nn.Module):
         # concatenation
         x = torch.cat([self.bn(input1), self.bn(input2)], dim=1)
         # 通道混洗
-        x = self.channel_shuffle(x)
+        x = self.downsample(x)
+
+        return x
+
+
+class FuseBlock(nn.Module):
+    def __init__(self, in_planes: int, out_planes: int):
+        super(FuseBlock, self).__init__()
+        self.branch1 = nn.Sequential(
+            nn.Conv2d(in_planes, out_planes, kernel_size=1, bias=False),
+            nn.BatchNorm2d(out_planes)
+        )
+        self.branch2 = nn.Sequential(
+            nn.Conv2d(in_planes, out_planes, kernel_size=3, padding=1, bias=False),
+            nn.BatchNorm2d(out_planes)
+        )
+
+    def forward(self, x):
         branch1 = self.branch1(x)
         branch2 = self.branch2(x)
-        branch3 = self.branch3(x)
-
         result = branch1 + branch2
-        result = result * branch3
+        return result
 
-        return F.silu(result, inplace=True)
+
+class Stream(nn.Module):
+    def __init__(self, in_planes: int, out_planes: int):
+        super(Stream, self).__init__()
+        self.fuse = FuseBlock(in_planes, out_planes)
+        self.sse = SSEBlock(in_planes, out_planes)
+
+    def forward(self, x):
+        fuse = self.fuse(x)
+        sse = self.sse(x)
+        result = F.silu(fuse + sse, inplace=True)
+        return result
+
+
+class ParNet(nn.Module):
+    def __init__(self, ds_channels: List[int], stream_nums: List[int], num_classes: int):
+        """
+        Args:
+            ds_channels: 列表类型, 存放除stream1分支上的下采样层外的所有下采样层输出通道数
+            stream_nums: 各个stream流中RepVGG SSE Block数
+            num_classes: 分类数量
+        """
+        super(ParNet, self).__init__()
+        self.ds1 = DownSampling(3, ds_channels[0])
+        self.ds2 = DownSampling(ds_channels[0], ds_channels[1])
+        self.ds3 = DownSampling(ds_channels[1], ds_channels[2])
+        self.ds4 = DownSampling(ds_channels[2], ds_channels[3])
+        self.ds5 = DownSampling(ds_channels[3], ds_channels[4])
+
+        self.stream1 = nn.Sequential(
+            *[Stream(ds_channels[1], ds_channels[1]) for _ in range(stream_nums[0])]
+        )
+        # 下采样层, 输出通道数等于stream2层输出通道数, 然后进行Fusion操作
+        self.stream1_downsample = DownSampling(ds_channels[1], ds_channels[2])
+
+        self.stream2 = nn.Sequential(
+            *[Stream(ds_channels[2], ds_channels[2]) for _ in range(stream_nums[1])]
+        )
+        # 将stream1和stream2进行融合, 输出通道数为stream3层输出通道数, 以便进行融合
+        self.stream2_fusion = FusionBlock(ds_channels[2], ds_channels[3])
+
+        self.stream3 = nn.Sequential(
+            *[Stream(ds_channels[3], ds_channels[3]) for _ in range(stream_nums[2])]
+        )
+        self.stream3_fusion = FusionBlock(ds_channels[3], ds_channels[3])
+
+        self.classifier = nn.Sequential(
+            nn.AdaptiveAvgPool2d((1, 1)),
+            nn.Flatten(),
+            nn.Linear(in_features=ds_channels[4], out_features=num_classes),
+            nn.Softmax(dim=1)
+        )
+
+    def forward(self, x):
+        # 连续两个下采样层
+        # ParNet-S
+        # N x 3 x 224 x 224
+        x = self.ds1(x)
+        # N x 64 x 112 x 112
+        x = self.ds2(x)
+        # N x 96 x 56 x 56
+
+        # 将第二个下采样结果传递给stream1
+        stream1 = self.stream1(x)
+        # N x 96 x 56 x 56
+        # stream1经过一连串RepVGG SSE Block后进入stream1-downsampling
+        stream1 = self.stream1_downsample(stream1)
+        # N x 192 x 28 x 28
+
+        # 进行第三个下采样
+        x = self.ds3(x)
+        # N x 192 x 28 x 28
+        # 第三个下采样结果传递给stream2
+        stream2 = self.stream2(x)
+        # N x 192 x 28 x 28
+        # 将stream1和stream2结果融合
+        stream2 = self.stream2_fusion(stream1, stream2)
+        # N x 384 x 14 x 14
+
+        # 进行第四个下采样
+        x = self.ds4(x)
+        # N x 384 x 14 x 14
+        # 第四个下采样结果传递给stream3
+        stream3 = self.stream3(x)
+        # N x 384 x 14 x 14
+        # 将stream2和stream3结果进行融合
+        stream3 = self.stream3_fusion(stream2, stream3)
+        # N x 384 x 7 x 7
+
+        # 上述结果融合过后进行最后一个下采样
+        x = self.ds5(stream3)
+        # N x 1280 x 7 x 7
+
+        # 分类器
+        result = self.classifier(x)
+        return result
+
+
+def create_model(model_name: str, num_classes: int):
+    stream_nums = [4, 5, 5]
+    if model_name == "ParNet-S":
+        ds_channels = [64, 96, 192, 384, 1280]
+    elif model_name == "ParNet-M":
+        ds_channels = [64, 128, 256, 512, 2048]
+    elif model_name == "ParNet-L":
+        ds_channels = [64, 160, 320, 640, 2560]
+    elif model_name == "ParNet-XL":
+        ds_channels = [64, 200, 400, 800, 3200]
+    else:
+        raise ValueError(f"Model name {model_name} not supported")
+    model = ParNet(ds_channels, stream_nums, num_classes)
+
+    return model
---
+++ 9-ParNet/train.py
@@ -0,0 +1,207 @@
+import os
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader
+from torchvision import transforms
+import argparse
+import random
+from datetime import datetime
+import time
+from datasets import MyDataset
+from utils import AverageMeter, accuracy
+from tqdm import tqdm
+from torch.utils.tensorboard import SummaryWriter
+from model.net import create_model
+
+parser = argparse.ArgumentParser("Pytorch ParNet Training")
+parser.add_argument('--epochs', type=int, default=100, help="number of total epochs to train")
+parser.add_argument('--batch_size', type=int, default=128, help="mini-batch size (default: 64)")
+parser.add_argument('--weight_decay', type=float, default=1e-4, help="weight decay (default: 1e-3)")
+parser.add_argument('--lr', '--learning_rate', type=float, default=0.1, help="initial learning rate")
+parser.add_argument('--print_freq', '-p', default=1, type=int, help="print frequency (default: 1)")
+parser.add_argument('--name', default='ParNet-S', type=str, help='name of experiment')
+parser.add_argument("--seed", type=int, default=33, help="random seed")
+parser.add_argument('--workers', type=int, default=8, help="max dataloader workers (per RANK in DDP mode)")
+parser.add_argument('--split_ratio', type=float, default=0.9, help="split ratio")
+parser.add_argument("--momentum", type=float, default=0.9, help="momentum")
+
+args = parser.parse_args()
+
+
+def seed_torch(seed=123):
+    # 设置随机数生成器的种子，确保实验可重复性
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+
+
+# 设置随机数种子, 确保实验可重复性
+seed_torch(args.seed)
+
+data_dir = "../data/train"
+
+# 设置训练设备
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+# 设置实验结果保存路径
+# exp/modelName_datetime_lr_wd
+model_name = args.name
+lr = args.lr
+weight_decay = args.weight_decay
+wd = format(weight_decay, '.1e')
+now = datetime.now().strftime(r"%mM_%dD_%HH")
+exp_path = "exp/{}_{}_lr{}_wd{}".format(now, model_name, lr, weight_decay)
+os.makedirs(exp_path, exist_ok=True)
+
+# 定义数据转换
+norm_mean = [0.485, 0.456, 0.406]
+norm_std = [0.229, 0.224, 0.225]
+
+normalize = transforms.Normalize(norm_mean, norm_std)
+
+data_transforms = {
+    "train": transforms.Compose([
+        transforms.Resize(256),  # 将短边缩放到256
+        transforms.CenterCrop(256),  # 中心裁剪
+        transforms.RandomCrop(224),  # 随机裁剪224x224大小的图像
+        transforms.RandomHorizontalFlip(p=0.5),  # 随机水平翻转
+        transforms.ToTensor(),  # 转为Tensor数据类型
+        transforms.Normalize(norm_mean, norm_std)  # 正则化
+    ]),
+    "val": transforms.Compose([
+        transforms.Resize(256),
+        transforms.RandomCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize(norm_mean, norm_std)
+    ])
+}
+
+train_set = MyDataset(data_dir, mode="train", transform=data_transforms["train"],
+                      split_ratio=args.split_ratio, rng_seed=args.seed)
+val_set = MyDataset(data_dir, mode="val", transform=data_transforms["val"],
+                    split_ratio=args.split_ratio, rng_seed=args.seed)
+
+train_loader = DataLoader(train_set, batch_size=args.batch_size, shuffle=True,
+                          num_workers=args.workers, pin_memory=True)
+val_loader = DataLoader(val_set, batch_size=args.batch_size, shuffle=True,
+                        num_workers=args.workers, pin_memory=True)
+
+
+# 训练一轮
+def train_one_epoch(model: nn.Module, optimizer: optim, train_loader: DataLoader, loss_fn: nn.CrossEntropyLoss):
+    model.train(True)
+    acc_recoder = AverageMeter()  # 记录准确率
+    loss_recoder = AverageMeter()  # 记录损失
+
+    for imgs, labels in tqdm(train_loader, desc="train"):
+        imgs, labels = imgs.to(device), labels.to(device)
+        outputs = model(imgs)
+        loss = loss_fn(outputs, labels)
+        loss_recoder.update(loss.item(), imgs.size(0))
+        acc = accuracy(outputs, labels)[0]
+        acc_recoder.update(acc.item(), imgs.size(0))
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+    losses = loss_recoder.avg
+    acc = acc_recoder.avg
+
+    return losses, acc
+
+
+def evaluation(model: nn.Module, val_loader: DataLoader, loss_fn: nn.CrossEntropyLoss):
+    model.eval()
+    acc_recoder = AverageMeter()
+    loss_recoder = AverageMeter()
+
+    with torch.no_grad():
+        for imgs, labels in tqdm(val_loader, desc="Evaluation"):
+            imgs, labels = imgs.to(device), labels.to(device)
+            outputs = model(imgs)
+            loss = loss_fn(outputs, labels)
+            loss_recoder.update(loss.item(), imgs.size(0))
+            acc = accuracy(outputs, labels)[0]
+            acc_recoder.update(acc.item(), imgs.size(0))
+
+    losses = loss_recoder.avg
+    acc = acc_recoder.avg
+
+    return losses, acc
+
+
+def train(model: nn.Module, train_loader: DataLoader, val_loader: DataLoader,
+          optimizer: optim, loss_fn: nn.CrossEntropyLoss, scheduler: optim.lr_scheduler.CosineAnnealingLR):
+    # 记录训练开始时间
+    start_time = time.time()
+    # 记录最佳准确率, 初始为-1
+    best_acc = -1
+    # 打开一个用于写入训练过程的信息文件
+    f = open(os.path.join(exp_path, "lr{}_wd{}.txt".format(lr, weight_decay)), "w")
+
+    for epoch in range(args.epochs):
+        train_losses, train_acc = train_one_epoch(model, optimizer, train_loader, loss_fn)
+        val_losses, val_acc = evaluation(model, val_loader, loss_fn)
+
+        if val_acc > best_acc:
+            best_acc = val_acc
+            state_dict = dict(epoch=epoch + 1, model=model.state_dict(), acc=val_acc)
+            pth_path = os.path.join(exp_path, "ckpt", "best.pth")
+            os.makedirs(os.path.dirname(pth_path), exist_ok=True)
+            torch.save(state_dict, pth_path)
+
+        # 更新学习率
+        scheduler.step()
+        # 定义要记录的训练信息标签
+        tags = ['train_losses', 'train_access', 'val_losses', 'val_access']
+        tb_writer.add_scalar(tags[0], train_losses, epoch + 1)
+        tb_writer.add_scalar(tags[1], train_acc, epoch + 1)
+        tb_writer.add_scalar(tags[2], val_losses, epoch + 1)
+        tb_writer.add_scalar(tags[3], val_acc, epoch + 1)
+
+        # 打印训练过程信息，以及将信息写入文件
+        if (epoch + 1) % args.print_freq == 0:  # print_freq指定为1 则每轮都打印
+            msg = "epoch:{} model:{} train loss:{:.2f} acc:{:.2f}  test loss{:.2f} acc:{:.2f}\n".format(
+                epoch + 1,
+                args.name,
+                train_losses,
+                train_acc,
+                val_losses,
+                val_acc,
+            )
+            print(msg)
+            f.write(msg)
+            f.flush()
+    # 输出训练结束后的最佳准确度和总训练时间
+    msg_best = "model:{} best acc:{:.2f}\n".format(args.name, best_acc)
+    time_elapsed = "training time: {}".format(time.time() - start_time)
+    print(msg_best)
+    f.write(msg_best)
+    f.write(time_elapsed)
+    f.close()
+
+
+if __name__ == '__main__':
+    tb_path = os.path.join(exp_path, "runs")
+    os.makedirs(tb_path, exist_ok=True)
+    tb_writer = SummaryWriter(log_dir=tb_path)
+
+    model = create_model(model_name, num_classes=2)
+    model = model.to(device)
+    optimizer = optim.SGD(  # 创建随机梯度下降 (SGD) 优化器
+        model.parameters(),
+        lr=lr,
+        momentum=args.momentum,
+        nesterov=True,
+        weight_decay=weight_decay,
+    )
+    loss_fn = nn.CrossEntropyLoss()
+    loss_fn.to(device)
+
+    scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=args.epochs)
+    train(model, train_loader, val_loader, optimizer, loss_fn, scheduler)
+    tb_writer.close()
